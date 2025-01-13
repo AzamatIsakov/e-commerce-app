3ce03e262c38e424495f8c4b1ee7dd9245f04f5b
Azamat Isakov <isoqov55azamat@gmail.com>
2025-01-14 00:07:31 +0500

    Add parse code

---
+++ utilities/JS/index.mjs
@@ -0,0 +1,16 @@
+import {
+  createJSONFile,
+  getDataFromJSON,
+  getItems,
+  transformToMyProductData,
+} from './utilities.mjs';
+
+const data = getDataFromJSON('./data/huseholdAappliances/6/default.json');
+
+const items = getItems(data);
+
+const newData = items.map(transformToMyProductData);
+
+createJSONFile('./data/huseholdAappliances/6/transformedData.json', {
+  items: newData,
+});
---
+++ utilities/JS/utilities.mjs
@@ -0,0 +1,51 @@
+import fs from 'fs';
+
+export const getDataFromJSON = (filePath) => {
+  try {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    const jsonData = JSON.parse(data);
+
+    // console.log(jsonData); // Данные из JSON
+
+    return jsonData;
+  } catch (error) {
+    console.error('Error reading the JSON file:', error);
+  }
+};
+
+export const createJSONFile = (filePath, data) => {
+  try {
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
+    console.log('JSON файл создан!');
+  } catch (error) {
+    console.error('Ошибка при создании файла:', error);
+  }
+};
+
+export const transformToMyProductData = (product) => {
+  const newProduct = {};
+
+  newProduct.id = product.catalogCard.id;
+  newProduct.title = product.catalogCard.title;
+  newProduct.price = {
+    min: product.catalogCard.minSellPrice,
+    max: product.catalogCard.minFullPrice,
+  };
+  newProduct.favorite = false;
+  newProduct.rating = product.catalogCard.rating;
+  newProduct.photos = product.catalogCard.photos.map(transformToMyPhotoData);
+  newProduct.discountInfo = product?.catalogCard?.discountInfo?.text || null;
+
+  return newProduct;
+};
+
+export const transformToMyPhotoData = (photoObj) => {
+  return {
+    id: photoObj.key,
+    link: photoObj.link.high,
+  };
+};
+
+export const getItems = (data) => {
+  return data.data.makeSearch.items;
+};

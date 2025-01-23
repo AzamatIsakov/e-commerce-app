cb1dfd35fc241c8a33568210786aeb12b0f9c267
Azamat Isakov <azamat.isakov@auto.uz>
2025-01-23 08:51:28 +0500

    Comment request to gemini

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,24 +1,24 @@
 import { ProductsSkeleton } from '@/components/shared/Skeleton'
 import { Products } from '@/shared'
 import { Suspense } from 'react'
-import { GoogleGenerativeAI } from '@google/generative-ai'
+// import { GoogleGenerativeAI } from '@google/generative-ai'
 
 const Home = async () => {
   console.log('Main page')
 
   // Access your API key as an environment variable (see "Set up your API key" above)
-  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || '')
-  console.log(process.env.GEMINI_API_KEY)
+  // const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || '')
+  // console.log(process.env.GEMINI_API_KEY)
 
   // The Gemini 1.5 models are versatile and work with most use cases
-  const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
+  // const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
 
-  const prompt = 'Расскажи что-нибудь'
+  // const prompt = 'Расскажи что-нибудь'
 
-  const result = await model.generateContent(prompt)
-  const response = await result.response
-  const text = response.text()
-  console.log(text)
+  // const result = await model.generateContent(prompt)
+  // const response = await result.response
+  // const text = response.text()
+  // console.log(text)
 
   return (
     <div className="container">

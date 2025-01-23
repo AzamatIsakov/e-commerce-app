f8629c9164023e7a58ce0b469f8ca53fab9fb0c5
Azamat Isakov <isoqov55azamat@gmail.com>
2025-01-23 23:48:33 +0500

    Add request with React Query in client component

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,3 +1,3 @@
 export { ProductCard } from './ProductCard'
 export { Products } from './Products'
-export { Header, Navbar } from './Layout'
+export { Header, Navbar, ReactQueryProvider } from './Layout'
--- services/Products.service.ts
+++ services/Products.service.ts
@@ -3,7 +3,9 @@ import { ProductDTO } from './Products.dto'
 
 export const getAllProducts = async (): Promise<ProductDTO> => {
   try {
-    const response = await axios.get(`${process.env.MAIN_URL}/api/products`)
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_MAIN_URL}/api/products`
+    )
 
     return response.data
   } catch (e) {

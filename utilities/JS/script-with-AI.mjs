5c016fae642bb89016f280b6d33462dea21ae237
Azamat Isakov <isoqov55azamat@gmail.com>
2025-02-16 04:20:51 +0500

    Add full data to 3 categories

--- utilities/JS/script-with-AI.mjs
+++ utilities/JS/script-with-AI.mjs
@@ -1,6 +1,13 @@
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import dotenv from 'dotenv'
 import fs from 'fs/promises' // Используем модуль fs/promises
+import { extractAndParseJSON } from './utilities.mjs'
+import {
+  getDescriptionPrompt,
+  getFeaturesPrompt,
+  getTagsPrompt,
+  getVariationPrompt
+} from './prompts.mjs'
 
 dotenv.config()
 
@@ -12,7 +19,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
 
 const generateDescription = async (title) => {
-  const prompt = `Привет, напиши описание для товара с названием "${title}". Укажи преимущества и особенности. Просто дай самый технический ответ. Придумай характеристики сам исходя из названия. Дай готовый результат, если у тебя есть сомнения, где надо уточнить, допустим материал, то придумай сам.`
+  const prompt = getDescriptionPrompt(title)
 
   const result = await model.generateContent(prompt)
   const response = await result.response
@@ -23,60 +30,77 @@ const generateDescription = async (title) => {
 
 const generateFeatures = async (title, description) => {
   try {
-    const prompt = `Сгенерируй массив из 7-15 объектов для характеристики товара. Товар: "${title}". Описание: "${description}". Каждый объект должен содержать свойства 'name' и 'value'. Характеристики должны быть логически связаны с названием и описанием товара. Примеры характеристик: ёмкость батареи, размер экрана, вес, материал корпуса, особенности камеры, производительность процессора. Отформатируй результат в виде JSON строку. Укажи все значения сам, цену не добавляй`
+    const prompt = getFeaturesPrompt(title, description)
 
     const result = await model.generateContent(prompt)
     const response = await result.response
-    const text = await response.text() // Добавляем await, чтобы получить результат
-
-    const startSliced = text.slice(8)
-    const endSliced = startSliced.slice(0, startSliced.length - 4)
-
-    const total = JSON.parse(endSliced)
+    const total = extractAndParseJSON(await response.text()) // Добавляем await, чтобы получить результат
 
     return total
   } catch (e) {
     console.log()
     console.log('Ошибка:', e)
     console.log()
+    console.log('Передышка')
+    await delay(10000)
+
     return []
   }
 }
 
 const generateVariation = async (title, description, price, features) => {
   try {
-    const prompt = `Сгенерируй массив из 3-10 объектов, каждый из которых представляет собой вариацию товара "${title}". Каждая вариация должна иметь уникальный идентификатор, случайную цену около ${price}, случайное количество на складе и набор опций, представленных массивом объектов. Структура объекта должна быть следующей:
-    {
-      "id": "uuid",
-      "price": 12345,
-      "stock": 500,
-      "options": [
-        {
-          "name": "Название опции (на русском)",
-          "value": "Значение опции (на английском)"
-        },
-        ...
-      ]
-    }
-    Пример товара: ${description}.
-    Общие характеристики товара для тебя как тоже входные данные: ${JSON.stringify(features)}
-    Дай мне готовый результат в виде JSON без лишних сообщений.
-    `
+    const prompt = getVariationPrompt(
+      title,
+      description,
+      price,
+      JSON.stringify(features)
+    )
 
     const result = await model.generateContent(prompt)
     const response = await result.response
-    const text = await response.text() // Добавляем await, чтобы получить результат
-
-    const startSliced = text.slice(8)
-    const endSliced = startSliced.slice(0, startSliced.length - 4)
-
-    const total = JSON.parse(endSliced)
+    const total = extractAndParseJSON(await response.text()) // Добавляем await, чтобы получить результат
 
     return total
   } catch (e) {
     console.log()
     console.log('Ошибка:', e)
     console.log()
+    console.log('Передышка')
+    await delay(10000)
+
+    return []
+  }
+}
+
+const generateTags = async (
+  title,
+  description,
+  features,
+  variations,
+  categories
+) => {
+  try {
+    const prompt = getTagsPrompt(
+      title,
+      description,
+      JSON.stringify(categories),
+      JSON.stringify(features),
+      JSON.stringify(variations)
+    )
+
+    const result = await model.generateContent(prompt)
+    const response = await result.response
+    const total = extractAndParseJSON(await response.text()) // Добавляем await, чтобы получить результат
+
+    return total
+  } catch (e) {
+    console.log()
+    console.log('Ошибка:', e)
+    console.log()
+    console.log('Передышка')
+    await delay(10000)
+
     return []
   }
 }
@@ -126,7 +150,7 @@ const updateJSONWithFeatures = async (readFilePath, writeFilePath) => {
       // Генерация описания
       item.features = await generateFeatures(item.title, item.description)
 
-      // Пауза перед следующим запросом (например, 1 секунда)
+      // Пауза перед следующим запросом (например, 2 секунды)
       await delay(2000)
     }
 
@@ -146,18 +170,18 @@ const updateJSONWithVariations = async (readFilePath, writeFilePath) => {
 
     // Обрабатываем каждый элемент
     for (const item of items) {
-      console.log(`Генерация характеристик для ${item.title}`)
-
-      // Генерация описания
-      item.variations = await generateVariation(
-        item.title,
-        item.description,
-        item.price.min,
-        item.features
-      )
-
-      // Пауза перед следующим запросом (например, 1 секунда)
-      await delay(2000)
+      if (!item.variations.length) {
+        console.log(`Генерация вариаций для ${item.title}`)
+        // Генерация описания
+        item.variations = await generateVariation(
+          item.title,
+          item.description,
+          item.price.min,
+          item.features
+        )
+        // Пауза перед следующим запросом (например, 1 секунда)
+        await delay(2000)
+      }
     }
 
     // Записываем результат
@@ -168,36 +192,72 @@ const updateJSONWithVariations = async (readFilePath, writeFilePath) => {
   }
 }
 
+const updateJSONWithTags = async (readFilePath, writeFilePath) => {
+  try {
+    // Читаем файл
+    const data = await fs.readFile(readFilePath, 'utf-8') // Используем fs/promises
+    const { items } = JSON.parse(data)
+
+    // Обрабатываем каждый элемент
+    for (const item of items) {
+      console.log(`Генерация тегов для ${item.title}`)
+
+      // Генерация описания
+      const generatedTags = await generateTags(
+        item.title,
+        item.description,
+        JSON.stringify(item.features),
+        JSON.stringify(item.variations),
+        JSON.stringify(item.categories)
+      )
+
+      item.tags = generatedTags.tags
+
+      // Пауза перед следующим запросом (например, 4 секунд)
+      await delay(4000)
+    }
+
+    // Записываем результат
+    await fs.writeFile(writeFilePath, JSON.stringify({ items }, null, 2))
+    console.log('JSON-файл успешно обновлен!')
+  } catch (error) {
+    console.error('Ошибка:', error)
+  }
+}
+
+//! Надо будеть проверять подстроки между собой и выявлять схожие как a.includes(b) || b.includes(a) и при этом можно разделить это всё на подстроки распарсить и проверять каждое слово на существование в подстроках
+//! как проверка "A16" на моделях "Samsung Galaxy A16"
+
 // !================================================================================
 
-await updateJSONWithDescriptions(
-  './data/electronics/phones/feature-phones/4/transformedData.json',
-  './data/electronics/phones/feature-phones/4/transformedWithDescriptionData.json'
-)
+// await updateJSONWithDescriptions(
+//   './data/electronics/phones/feature-phones/4/transformedData.json',
+//   './data/electronics/phones/feature-phones/4/transformedWithDescriptionData.json'
+// )
 
-console.log('Пауза <||>')
-await delay(10000)
-
-await updateJSONWithDescriptions(
-  './data/electronics/phones/feature-phones/5/transformedData.json',
-  './data/electronics/phones/feature-phones/5/transformedWithDescriptionData.json'
-)
+// console.log('Пауза <||>')
+// await delay(10000)
 
 // !================================================================================
 
 // await updateJSONWithFeatures(
-//   './data/electronics/phones/smartphones/1/transformedWithDescriptionData.json',
-//   './data/electronics/phones/smartphones/1/transformedData.json'
+//   './data/electronics/phones/landline-phones/1/transformedWithDescriptionData.json',
+//   './data/electronics/phones/landline-phones/1/dataWithFeatures.json'
 // )
 
 // console.log('Пауза <||>')
 // await delay(10000)
+
+// await updateJSONWithFeatures(
+//   './data/electronics/phones/landline-phones/2/transformedWithDescriptionData.json',
+//   './data/electronics/phones/landline-phones/2/dataWithFeatures.json'
+// )
 
 // !================================================================================
 
 // await updateJSONWithVariations(
-//   './data/electronics/phones/smartphones/1/transformedData.json',
-//   './data/electronics/phones/smartphones/1/data.json'
+//   './data/electronics/phones/landline-phones/1/dataWithFeatures.json',
+//   './data/electronics/phones/landline-phones/1/data.json'
 // )
 
 // console.log()
@@ -206,38 +266,53 @@ await updateJSONWithDescriptions(
 // console.log()
 
 // await updateJSONWithVariations(
-//   './data/electronics/phones/smartphones/2/transformedData.json',
-//   './data/electronics/phones/smartphones/2/data.json'
-// )
-
-// console.log()
-// console.log('Пауза <||>')
-// await delay(10000)
-// console.log()
-
-// await updateJSONWithVariations(
-//   './data/electronics/phones/smartphones/3/transformedData.json',
-//   './data/electronics/phones/smartphones/3/data.json'
-// )
-
-// console.log()
-// console.log('Пауза <||>')
-// await delay(10000)
-// console.log()
-
-// await updateJSONWithVariations(
-//   './data/electronics/phones/smartphones/4/transformedData.json',
-//   './data/electronics/phones/smartphones/4/data.json'
-// )
-
-// console.log()
-// console.log('Пауза <||>')
-// await delay(10000)
-// console.log()
-
-// await updateJSONWithVariations(
-//   './data/electronics/phones/smartphones/5/transformedData.json',
-//   './data/electronics/phones/smartphones/5/data.json'
+//   './data/electronics/phones/landline-phones/2/dataWithFeatures.json',
+//   './data/electronics/phones/landline-phones/2/data.json'
 // )
 
 // !================================================================================
+
+// await updateJSONWithTags(
+//   './data/electronics/phones/landline-phones/1/data.json',
+//   './data/electronics/phones/landline-phones/1/products.json'
+// )
+
+// console.log()
+// console.log('Пауза <||>')
+// await delay(10000)
+// console.log()
+
+// await updateJSONWithTags(
+//   './data/electronics/phones/landline-phones/2/data.json',
+//   './data/electronics/phones/landline-phones/2/products.json'
+// )
+
+// console.log()
+// console.log('Пауза <||>')
+// await delay(10000)
+// console.log()
+
+// await updateJSONWithTags(
+//   './data/electronics/phones/smartphone-accessories/3/data.json',
+//   './data/electronics/phones/smartphone-accessories/3/products.json'
+// )
+
+// console.log()
+// console.log('Пауза <||>')
+// await delay(10000)
+// console.log()
+
+// await updateJSONWithTags(
+//   './data/electronics/phones/smartphone-accessories/4/data.json',
+//   './data/electronics/phones/smartphone-accessories/4/products.json'
+// )
+
+// console.log()
+// console.log('Пауза <||>')
+// await delay(10000)
+// console.log()
+
+// await updateJSONWithTags(
+//   './data/electronics/phones/smartphone-accessories/5/data.json',
+//   './data/electronics/phones/smartphone-accessories/5/products.json'
+// )
